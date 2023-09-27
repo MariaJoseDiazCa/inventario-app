86831f208f81a4912532fdc8a878c5fd23ea3cfb
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2023-09-26 23:12:20 -0300

    ADD: product listing component

---
+++ src/app/producto-lista/producto-lista.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-lista',
+  templateUrl: './producto-lista.component.html',
+})
+export class ProductoListaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
